e4970d528c818a121291a231244414aaa228e7dc
Ananya Gupta <ananyag2806@gmail.com>
2021-07-11 18:43:18 +0530

    npm install chessboardjsx

---
+++ src/conponents/Chessboard.js
@@ -0,0 +1,37 @@
+// import React from 'react'
+// import Chessboard from './Assets/Chessboard.png'
+
+// import pawnB from './Assets/pawnB.png'
+// import pawnW from './Assets/pawnW.png'
+// import bishopB from './Assets/bishopB.png'
+// import bishopW from './Assets/bishopW.png'
+// import knightB from './Assets/knightB.png'
+// import knightW from './Assets/knightW.png'
+// import rookB from './Assets/rookB.png'
+// import rookW from './Assets/rookW.png'
+// import kingB from './Assets/kingB.png'
+// import kingW from './Assets/kingW.png'
+// import queenB from './Assets/queenB.png'
+// import queenW from './Assets/queenW.png'
+
+
+// function setPiece(pos){
+    
+// }
+
+// function chessboard() {
+//     return (
+//         <div>
+//             <img src={Chessboard} />
+//             <img src={pawnB} />
+//             <img src={bishopB} />
+//             <img src={rookB} />
+//             <img src={kingB} />
+//             <img src={queenB} />
+//             <img src={knightB} />
+
+//         </div>
+//     )
+// }
+
+// export default chessboard
